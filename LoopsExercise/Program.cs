b4edbd13705db86787e22eea879820acfaf13ba0
megamagicmonkey <megamagicmonkey@gmail.com>
2021-08-24 11:51:55 -0500

    Initial commit

---
+++ LoopsExercise/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace LoopsExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 1000000; i++)
+            {
+                Console.WriteLine($"{i} This code is so dull.");
+            }
+        }
+    }
+}
